50c65c86e2553f3e57e76c41ae59952c87e2af4b
Kim, Hirokuni <yangkookkim@gmail.com>
2018-02-05 22:56:00 +0900

    Add client

--- consumer/src/zoo_app/core.clj
+++ consumer/src/zoo_app/core.clj
@@ -5,12 +5,26 @@
 
 (def animal-service-url "http://localhost:9999")
 
-(defn get-animal []
-  (http/get animal-service-url
-            (fn [{:keys [status headers body error]}] ;; asynchronous response handling
-              (if error
-                (println "Failed, exception is " error)
-                (println "Async HTTP GET: " status)))))
+(defn ->ok [json-str]
+  (let [body (json/read-str json-str)]
+    (into {}
+          (for [[k v] body]
+            [(keyword k) v]))))
+
+(defn ->not-found []
+  {:error "not found"})
+
+(defn ->error []
+  {:error "something bad happend"})
+
+(defn get-alligator-by-name
+  [name]
+  (let [res (http/get (format "%s/alligators/%s" animal-service-url name) {:as :text})
+        status (:status @res)]
+    (case status
+      200 (->ok (:body @res))
+      404 (->not-found)
+      (->error))))
 
 (defn -main
   "I don't do a whole lot ... yet."
